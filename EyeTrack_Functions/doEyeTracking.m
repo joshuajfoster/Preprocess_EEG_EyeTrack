57159712dda3b1b4af4f322e20d2791c255526f4
Joshua Foster <joshua.james.foster@gmail.com>
2018-04-03 11:56:23 -0500

    updated runEyeTrack and doEyeTracking
    Moved checks that settings match eye tracking file to doEyeTracking. Also moved artifact rjection out of doEyeTracking.

--- EyeTrack_Functions/doEyeTracking.m
+++ EyeTrack_Functions/doEyeTracking.m
@@ -6,6 +6,19 @@ function eyeData = doEyeTracking(eye,settings)
 % eye: eye structure with all eye tracking data
 % settings: settings structure with eye tracking settings specified
 
+%% check that  eye tracker settings match what is specified in settings file
+
+% check the specified sampling rate is correct
+if settings.sRate ~= eye.RECORDINGS(1).('sample_rate')
+    error('specified sampling rate does not match data file.')
+end
+
+% check the specified recording mode is correct
+recordingMode = getRecordingMode(eye);
+if ~strcmp(recordingMode,settings.recordingMode) % if these strings don't match...
+    error('specified recording mode does not match data file.')
+end
+
 %% Grab the relevant data from the eye stucture
 
 % sampling rate
@@ -30,32 +43,10 @@ eyeData.hx = [eye.FSAMPLE(:).hx]; % head referenced x coords
 eyeData.hy = [eye.FSAMPLE(:).hy]; % head referenced y coords
 eyeData.pa = [eye.FSAMPLE(:).pa]; % head referenced pupil size / area
 
-% get distance to eye tracker if using remote mode, otherwise store vector of nans
-if strcmp(settings.recordingMode,'RemoteMode_Monocular') | strcmp(settings.recordingMode,'RemoteMode_Binocular')
-    eyeData.dist = (double((eye.FSAMPLE(:).hdata(3,:))))./100; % 3rd row is distance, divide by 100 to scale to cm
-else
-    eyeData.dist = nan(size(eyeData.sampleTimes)); % same size as eyeData.sampleTimes
-end
-
 %% Segment data
 eyeData.trial = segment_eyetracking(eyeData,settings); 
 
 %% calculate eye position in degrees of vis angle from fixation
-
-% if data collected in remote mode
-if strcmp(settings.recordingMode,'RemoteMode_Monocular') | strcmp(settings.recordingMode,'RemoteMode_Binocular')
-    
-    % check that settings.cam2screenDist exists, then use it!
-    if isfield(settings,'cam2screenDist')
-        cam2screenDist = settings.cam2screenDist;
-    else % otherwise estimate it from viewing dist and head2cam dist
-        cam2screenDist = approx_cam2screenDist(settings.viewDist,eyeData.dist);
-    end
-    
-    % calculate degrees of visual angle
-    [eyeData.trial.xDeg,eyeData.trial.yDeg] = pix2deg_remoteMode(eyeData.trial.gx,eyeData.trial.gy,eyeData.trial.dist,...
-                                                cam2screenDist,settings.monitor.xPixels,settings.monitor.yPixels,settings.monitor.pxSize);
-end
 
 % if data collected with the chin rest...
 if strcmp(settings.recordingMode,'ChinRest_Monocular') | strcmp(settings.recordingMode,'ChinRest_Binocular')
@@ -63,23 +54,13 @@ if strcmp(settings.recordingMode,'ChinRest_Monocular') | strcmp(settings.recordi
     % calculate degrees of visual angle
     [eyeData.trial.xDeg,eyeData.trial.yDeg] = pix2deg_chinRest(eyeData.trial.gx,eyeData.trial.gy,...
                                                 settings.monitor.xPixels,settings.monitor.yPixels,settings.monitor.pxSize,settings.viewDist);
+else
+    errror('Do not use remote mode. It sucks!')
 end
 
-
-%% Artifact rejection: mark bad data
-
-% mark bad data based on eyelink parser
-[eyeData.arf.parserBlinks, eyeData.arf.parserSaccs] = markParserArtifacts(eyeData);
-
-% run our own check for artifacts
-[eyeData.arf.missingPupil, eyeData.arf.saccadeX, eyeData.arf.saccadeY] = runArtRejection_EyeTrack(eyeData,settings);
-
-% print artifact summary
-fprintf(sprintf('Parser Blink Rate: %.2f \n',sum(eyeData.arf.parserBlinks)./length(eyeData.arf.parserBlinks)))
-fprintf(sprintf('Parser Saccade Rate: %.2f \n',sum(eyeData.arf.parserSaccs)./length(eyeData.arf.parserSaccs)))
-fprintf(sprintf('Calculated nonsensical position vals: %.2f \n',sum(eyeData.arf.missingPupil)./length(eyeData.arf.missingPupil)))
-fprintf(sprintf('Calculated horizontal saccades: %.2f \n',sum(eyeData.arf.saccadeX)./length(eyeData.arf.saccadeX)))
-fprintf(sprintf('Calculated vertical saccades: %.2f \n',sum(eyeData.arf.saccadeY)./length(eyeData.arf.saccadeY)))
+%% do baseline corection
+eyeData.trial.xDeg_bl = doBaseline_EyeTrack(eyeData.trial.xDeg,eyeData.trial.times,settings.seg.bl_start,settings.seg.bl_end);
+eyeData.trial.yDeg_bl = doBaseline_EyeTrack(eyeData.trial.yDeg,eyeData.trial.times,settings.seg.bl_start,settings.seg.bl_end);
 
 %% Remove continuos data from eyeData structure
 eyeData = rmfield(eyeData,'gx');
@@ -87,4 +68,3 @@ eyeData = rmfield(eyeData,'gy');
 eyeData = rmfield(eyeData,'hx');
 eyeData = rmfield(eyeData,'hy');
 eyeData = rmfield(eyeData,'pa');
-eyeData = rmfield(eyeData,'dist');
--- runEyeTrack.m
+++ runEyeTrack.m
@@ -11,29 +11,16 @@ fprintf('Subject:\t%d\n',sn)
 % Everything that you'd want to change is specified in the EyeTrack_Settings script
 settings = EyeTrack_Settings;
 
-%% preprocess eye track data and save file (no segmentation)
-fname = [num2str(sn),settings.dir.raw_filename];
-f_dir_name = [settings.dir.raw_data_path,fname,'.edf'];
-if ~exist(f_dir_name)
-    error(['Cannot find the file at ',f_dir_name])
-end
-eye = edfmex([settings.dir.raw_data_path,fname,'.edf']); % read in edf file
-
-% check the specified sampling rate is correct
-if settings.sRate ~= eye.RECORDINGS(1).('sample_rate')
-    error('specified sampling rate does not match data file.')
-end
-
-% check the specified recording mode is correct
-recordingMode = getRecordingMode(eye);
-if ~strcmp(recordingMode,settings.recordingMode) % if these strings don't match...
-    error('specified recording mode does not match data file.')
-end
-
-% save the data file
-save([settings.dir.processed_data_path,fname,'.mat'],'eye')
+%% convert the edf file to a mat file (and save)
+edf_name = [num2str(sn),settings.dir.raw_filename];
+eye = edf2mat(settings.dir.raw_data_path,edf_name,settings.dir.processed_data_path);
 
 %% preprocess eyetracking data and save file
 eyeData = doEyeTracking(eye,settings);
+
+%% artifact rejection
+arfSettings = EyeTrack_ArtRejectSettings;
+eyeData = runArtRejection_EyeTrack(eyeData,arfSettings); 
+
 eyeData.settings = settings; % save settings to eyeData structure
 save([settings.dir.processed_data_path,num2str(sn),'_EYE_SEG_',settings.dir.raw_filename,'.mat'],'eyeData')
