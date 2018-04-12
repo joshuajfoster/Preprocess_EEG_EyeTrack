793bab1a629bd3902909aab32077e8f62f69a76b
Joshua Foster <joshua.james.foster@gmail.com>
2018-04-12 10:54:34 -0500

    fixed checkForMissingPupil

--- EyeTrack_Functions/checkForMissingPupil.m
+++ EyeTrack_Functions/checkForMissingPupil.m
@@ -13,7 +13,7 @@ function [mpx mpy] = checkForMissingPupil(xgaze,ygaze)
 mpx = zeros(size(xgaze));
 mpy = zeros(size(ygaze));
 
-pmx(xgaze > 10000) = 1;
-pmy(ygaze > 10000) = 1;
+mpx(xgaze > 10000) = 1;
+mpy(ygaze > 10000) = 1;
 
 end
