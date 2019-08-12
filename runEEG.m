d1bdb921af2228467fdba7ea8a208f4619ca91f9
Joshua Foster <joshua.james.foster@gmail.com>
2019-08-12 12:06:05 -0400

    update doEEG, doERPs, and runEEG
    now save files in doEEG and doERPs rather than in runEEG. doEEG now calls the new function "dropElectrodes/m"

--- runEEG.m
+++ runEEG.m
@@ -20,19 +20,11 @@ fprintf('Subject:\t%d\n',sn)
 %% preprocess EEG data and save file (no segmentation)
 tic
 eeg = doEEG(sn,settings.dir.raw_data_path,settings.dir.raw_filename,settings);
-fprintf('saving EEG file.. \n')
-eegName = [settings.dir.processed_data_path,num2str(sn),'_EEG_',settings.dir.raw_filename,'.mat'];
-eeg.droppedElectrodes = settings.droppedElectrodes; % only save dropped electrodes, other settings apply to segmentation
-save(eegName,'eeg');% note: faster to load file that isn't v7.3 but there is a size limitation
 toc % report time to process EEG data
 
 %% do ERPs
 tic
 erp = eeg; clear eeg; % pass eeg data to erp structure
-erp = doERPs(erp,settings); % doERP pipeline
-fprintf('saving ERP file.. \n')
-erpName = [settings.dir.processed_data_path,num2str(sn),'_EEG_SEG_',settings.dir.raw_filename,'.mat'];
-erp.settings = settings; % add settings to erp struct before saving
-save(erpName,'erp'); % note: faster to load file that isn't v7.3 but there is a size limitation
+erp = doERPs(sn,erp,settings); % doERP pipeline
 toc % report time to doERPs
 clear erp
